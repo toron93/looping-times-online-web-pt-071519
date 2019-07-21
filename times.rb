ae001aa3dd1563712d5025990543b729eaea6f3e
Toron Seigler <toronseigler9@gmail.com>
2019-07-21 23:42:53 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,3 +1,8 @@
-# 7.times do |Wingardium Leviosa|
+# imes do |Wingardium Leviosa|
 #   puts "Wingardium Leviosa"
 # end
+def using_times
+  7.times do
+    puts "Wingardium Leviosa"
+    end
+  end 
